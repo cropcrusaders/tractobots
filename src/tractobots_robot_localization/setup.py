5d538d7a4754e7a50c31920e4ca4203e86d500ab
bassyboi <165348445+bassyboi@users.noreply.github.com>
2025-04-19 18:17:11 +1000

    Create setup.py

---
+++ src/tractobots_robot_localization/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+package_name = 'tractobots_robot_localization'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],  # the Python module directory
+    data_files=[
+        # ament index
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        # install the ROS package manifest
+        ('share/' + package_name, ['package.xml']),
+        # install launch & param folders so users can `ros2 launch` directly
+        ('share/' + package_name + '/launch', ['launch/ekf.launch.py',
+                                               'launch/navsat.launch.py']),
+        ('share/' + package_name + '/params', ['params/ekf_localization.yaml',
+                                               'params/navsat_transform.yaml']),
+    ],
+    install_requires=[
+        'setuptools',
+        'tf-transformations'      # needed by pose_transformer
+    ],
+    zip_safe=True,
+    maintainer='Kyler Laird',
+    maintainer_email='KylerLaird@todo.com',
+    description='Helper nodes (GPS parser, IMU republisher, pose transformer) '
+                'that wrap the robot_localization stack for Tractobots.',
+    license='GPLv3',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'gps_parser = tractobots_robot_localization.gps_parser:main',
+            'imu_publisher = tractobots_robot_localization.imu_publisher:main',
+            'pose_transformer = tractobots_robot_localization.pose_transformer:main',
+        ],
+    },
+)
