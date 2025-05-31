a576f08a60c3f10938b178303460323adac9a3f5
bassyboi <165348445+bassyboi@users.noreply.github.com>
2025-05-31 18:05:42 +1000

    Fix AgIsoStack event dispatcher and to_string

--- src/AgIsoStackPlusPlus/include/isobus/utility/to_string.hpp
+++ src/AgIsoStackPlusPlus/include/isobus/utility/to_string.hpp
@@ -6,8 +6,19 @@
 namespace isobus {
 
 template<typename T>
-std::string to_string(T value) {
+std::string to_string(T value)
+{
     return std::to_string(value);
+}
+
+inline std::string to_string(const std::string &value)
+{
+    return value;
+}
+
+inline std::string to_string(const char *value)
+{
+    return std::string(value);
 }
 
 } // namespace isobus
